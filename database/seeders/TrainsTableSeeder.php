32278a8d6bab7974259c4cbd80685ec771448f3d
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-06-08 19:33:35 +0200

    changed: date time using faker

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -15,13 +15,15 @@ class TrainsTableSeeder extends Seeder
    * @return void
    */
   public function run(Faker $faker){
-    for($i = 0; $i < 20; $i++){
+    for($i = 0; $i < 50; $i++){
       $new_train = new Train();
       $new_train->company = $faker->randomElement(['Italo', 'Trenitalia', 'Frecciarossa', 'FlixTrain', 'SNCF', 'OUIGO', 'Deutche Bahn', 'SBB', 'OBB', 'WestBahn', 'iryo', 'Renfe', 'DSB', 'SNCB']);
       $new_train->starting_station = $faker->city();
       $new_train->arriving_station = $faker->city();
-      $new_train->starting_time = $faker->date() . ' '. $faker->time();
-      $new_train->arriving_time = $faker->date() . ' '. $faker->time();
+      $new_train->starting_time = $faker->dateTimeThisMonth('+12 days');
+      do{
+        $new_train->arriving_time = $faker->dateTimeThisMonth('+12 days');
+      }while($new_train->starting_time < $faker->dateTimeThisMonth('+12 days'));
       $new_train->train_code = $faker->isbn10();
       $new_train->number_of_carriages = rand(3,10);
       $new_train->is_in_time = rand(0,1);
