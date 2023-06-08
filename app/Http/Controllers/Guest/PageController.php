6efab3e848c57d83ed5e0a27134e57d31d8ba18a
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-06-08 19:34:07 +0200

    add: output of trains

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Train;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
   public function index(){
-    return view('home');
+    $trains = Train::orderBy('starting_time')->get();
+    return view('home', compact('trains'));
   }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,43 @@
 
 @section('content')
   <main>
-    <h1>Home</h1>
+    <div class="pg-main-wrapper px-5 py-5">
+      <h1 class="text-center">Lista dei treni</h1>
+
+      <div class="pg-table-container mt-5">
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">#ID</th>
+              <th scope="col">Compagnia</th>
+              <th scope="col">Stazione di partenza</th>
+              <th scope="col">Stazione di arrivo</th>
+              <th scope="col">Orario di partenza</th>
+              <th scope="col">Orario di arrivo</th>
+              <th scope="col">Codice treno</th>
+              <th scope="col">Numero di carrozze</th>
+              <th scope="col">Stato del treno</th>
+              <th scope="col">Cancellato</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach ($trains as $train)
+            <tr>
+              <th scope="row">{{$train->id}}</th>
+              <td>{{$train->company}}</td>
+              <td>{{$train->starting_station}}</td>
+              <td>{{$train->arriving_station}}</td>
+              <td>{{$train->starting_time}}</td>
+              <td>{{$train->arriving_time}}</td>
+              <td>{{$train->train_code}}</td>
+              <td>{{$train->number_of_carriages}}</td>
+              <td class="{{$train->is_in_time === 0 ? 'text-danger' : 'text-success'}}">{{$train->is_in_time === 0 ? 'In ritardo' : 'In orario'}}</td>
+              <td class="{{$train->is_cancelled === 0 ? 'text-danger' : 'text-success'}}">{{$train->is_cancelled === 0 ? 'Cancellato' : 'Non cancellato'}}</td>
+            </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+    </div>
   </main>
 @endsection
